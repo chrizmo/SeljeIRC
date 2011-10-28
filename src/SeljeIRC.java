89bc084b6cd52549c0fe4069d4b892595fc4b052
Hallvard <hallvard.westman@gmail.com>
2011-10-28 13:47:50 +0200

    layout pops up

--- src/SeljeIRC.java
+++ src/SeljeIRC.java
@@ -1,7 +1,29 @@
+import SeljeIRC.IrcLayout;
 import javax.swing.*;
 
+
 /**
- * Tulltull!
+ * 
  * @author Hallvard Westman
  */
 
+public class SeljeIRC extends JFrame{
+
+public static void main(String[] args) {
+    
+       IrcLayout irc = new IrcLayout();
+       irc.createLayout();
+       JFrame f = new JFrame();
+       
+       
+     
+       f.getContentPane().add(irc);
+       
+       f.setVisible(true);
+       f.pack();
+       
+       f.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+       
+        
+ }
+}
