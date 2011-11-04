6505e51c35917b36ca7a174598801c8d8128ee13
Jon Arne Westgaard <jon@wstgrd.com>
2011-11-04 10:54:10 +0100

    Implemented JerkLib

--- src/jerklib/examples/Example.java
+++ src/jerklib/examples/Example.java
@@ -1,43 +1,43 @@
-package SeljeIRC;
+package jerklib.examples;
 
- 
 import jerklib.ConnectionManager;
 import jerklib.Profile;
 import jerklib.Session;
 import jerklib.events.*;
 import jerklib.events.IRCEvent.Type;
 import jerklib.listeners.IRCEventListener;
- 
+
 /**
- * @author mohadib A simple example that demonsrates how to use JerkLib
+ *  A simple example that demonsrates how to use JerkLib
+ *  @author mohadib 
  */
-public class ConnectToServer implements IRCEventListener
+public class Example implements IRCEventListener
 {
 	private ConnectionManager manager;
- 
-	public ConnectToServer()
+	
+	public Example()
 	{
 		/*
 		 * ConnectionManager takes a Profile to use for new connections.
 		 */
-		manager = new ConnectionManager(new Profile("JADDA"));
- 
+		manager = new ConnectionManager(new Profile("scripy"));
+		
 		/*
 		 * One instance of ConnectionManager can connect to many IRC networks.
 		 * ConnectionManager#requestConnection(String) will return a Session object.
 		 * The Session is the main way users will interact with this library and IRC
 		 * networks
 		 */
-		Session session = manager.requestConnection("irc.homelien.no");
- 
+		Session session = manager.requestConnection("irc.freenode.net");
+		
 		/*
 		 * JerkLib fires IRCEvents to notify users of the lib of incoming events
 		 * from a connected IRC server.
 		 */
 		session.addIRCEventListener(this);
- 
+
 	}
- 
+
 	/*
 	 * This method is for implementing an IRCEventListener. This method will be
 	 * called anytime Jerklib parses an event from the Session its attached to.
@@ -46,32 +46,31 @@ public class ConnectToServer implements IRCEventListener
 	 */
 	public void receiveEvent(IRCEvent e)
 	{
+		
+		
 		if (e.getType() == Type.CONNECT_COMPLETE)
 		{
-			e.getSession().join("#lindir");
- 
+			e.getSession().join("#jerklib");
 		}
 		else if (e.getType() == Type.CHANNEL_MESSAGE)
 		{
 			MessageEvent me = (MessageEvent) e;
-			System.out.println("<" + me.getNick() + ">"+ ":" + me.getMessage());
+			System.out.println(me.getNick() + ":" + me.getMessage());
 		}
 		else if (e.getType() == Type.JOIN_COMPLETE)
 		{
 			JoinCompleteEvent jce = (JoinCompleteEvent) e;
- 
-			/* say hello and version number */
-			jce.getChannel().say("Hei!! Denne IRC-klienten er laget ved hjelp av JerkLib!!! :D ");
-			jce.getChannel().say("Woooooop!");
+			/* say hello */
+			jce.getChannel().say("Hello from Jerklib!");
 		}
 		else
 		{
 			System.out.println(e.getType() + " " + e.getRawEventData());
 		}
 	}
- 
+
 	public static void main(String[] args)
 	{
-		new ConnectToServer();
+		new Example();
 	}
 }
